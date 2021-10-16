85798396c674b1f7637589ec564e363c50eac4d2
Artus U <artusrock@hotmail.com>
2021-10-16 15:20:29 +0100

    refactoring components

--- src/App.js
+++ src/App.js
@@ -1,78 +1,7 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import CoinGecko from "coingecko-api";
-import authService from "./services/authentication";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function Home() {
-  const [coins, setCoins] = useState(null);
-
-  useEffect(() => {
-    const CoinGeckoClient = new CoinGecko();
-
-    async function pingAPI() {
-      return await CoinGeckoClient.ping();
-    }
-
-    async function fetchCoinList() {
-      const response = await CoinGeckoClient.coins.all();
-      setCoins(response.data);
-      return response;
-    }
-
-    pingAPI();
-    fetchCoinList();
-  }, []);
-
-  return (
-    <div>
-      {coins && (
-        <ul>
-          {coins.map((coin) => {
-            return <li key={coin.id}>{coin.id}</li>;
-          })}
-        </ul>
-      )}
-    </div>
-  );
-}
-
-function About() {
-  return <div>About</div>;
-}
-
-function Navbar() {
-  const [user, setUser] = useState(() => {
-    return authService.getUser();
-  });
-
-  function handleLogout() {
-    authService.logout();
-    setUser(null);
-  }
-
-  function handleLogin() {
-    const user = authService.login();
-    setUser(user);
-  }
-
-  return (
-    <nav>
-      {user ? (
-        <button onClick={handleLogout}>Logout</button>
-      ) : (
-        <button onClick={handleLogin}>Login</button>
-      )}
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
 
 export default function App() {
   return (
@@ -82,9 +11,6 @@ export default function App() {
         <Switch>
           <Route path="/" exact>
             <Home />
-          </Route>
-          <Route path="/about">
-            <About />
           </Route>
         </Switch>
       </div>
---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+import CoinGecko from "coingecko-api";
+
+export function Home() {
+  const [coins, setCoins] = useState(null);
+
+  useEffect(() => {
+    const CoinGeckoClient = new CoinGecko();
+
+    async function pingAPI() {
+      return await CoinGeckoClient.ping();
+    }
+
+    async function fetchCoinList() {
+      const response = await CoinGeckoClient.coins.all();
+      setCoins(response.data);
+      return response;
+    }
+
+    pingAPI();
+    fetchCoinList();
+  }, []);
+
+  return (
+    <div>
+      {coins && (
+        <ul>
+          {coins.map((coin) => {
+            return <li key={coin.id}>{coin.id}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+}
