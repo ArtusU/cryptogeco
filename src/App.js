6482cfa9aa642291ea5489cf4388860b87a45d1e
Artus U <artusrock@hotmail.com>
2021-10-16 12:19:01 +0100

    testing CoinGecoClientAPI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import CoinGecko from "coingecko-api";
+
 function App() {
-  return <div>Hello You.</div>;
+  const [coins, setCoins] = useState(null);
+
+  useEffect(() => {
+    const CoinGecoClient = new CoinGecko();
+    async function pingAPI() {
+      return await CoinGecoClient.ping();
+    }
+
+    async function fetchCoinList() {
+      const response = await CoinGecoClient.coins.all();
+      setCoins(response.data);
+    }
+
+    pingAPI();
+    fetchCoinList();
+  }, []);
+
+  return (
+  <div>
+    {coins && (
+      <ul>
+        {coins.map((coin) => {
+          return <li key={coin.id}>{coin.name}</li>;
+        })}
+      </ul>
+    )}
+  </div>
+  )
 }
 
 export default App;
